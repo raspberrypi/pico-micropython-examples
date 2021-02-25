8b1bf5f30cddd673ba70ddfd4303bd85dd657210
7west <33707859+7west@users.noreply.github.com>
2021-02-24 21:56:51 -0500

    Update code, diagram, and README
    Fixed things several errors.

--- uart/uart.py
+++ uart/uart.py
@@ -7,9 +7,9 @@ uart0 = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
 
 txData = b'hello world\n\r'
 uart1.write(txData)
-time.sleep(1)
+time.sleep(0.1)
 rxData = bytes()
 while uart0.any() > 0:
-    rxData += uart0.read()
-    
+    rxData += uart0.read(1)
+
 print(rxData.decode('utf-8'))
