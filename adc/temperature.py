069bbbe7b4ee35c66e6924caa54552fb12765b0d
ryakosh <ryakosh@gmail.com>
2021-12-14 22:22:35 +0330

    Change subtraction to addition to properly calculate temperature

--- adc/temperature.py
+++ adc/temperature.py
@@ -9,6 +9,6 @@ while True:
     
     # The temperature sensor measures the Vbe voltage of a biased bipolar diode, connected to the fifth ADC channel
     # Typically, Vbe = 0.706V at 27 degrees C, with a slope of -1.721mV (0.001721) per degree. 
-    temperature = 27 - (reading - 0.706)/0.001721
+    temperature = 27 + (reading - 0.706)/0.001721
     print(temperature)
     utime.sleep(2)
