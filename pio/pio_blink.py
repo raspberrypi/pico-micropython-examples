18ee8b84e520dede9c2da1c44bfbafa691c85022
Toby Roberts <29973222+tobybroberts@users.noreply.github.com>
2023-02-28 14:06:31 +0000

    Revert to Pin 25 from LED

--- pio/pio_blink.py
+++ pio/pio_blink.py
@@ -19,8 +19,8 @@ def blink():
     nop()          [31]
     wrap()
 
-# Instantiate a state machine with the blink program, at 2000Hz, with set bound to Pin(25) (LED on the rp2 board)
-sm = rp2.StateMachine(0, blink, freq=2000, set_base=Pin("LED"))
+# Instantiate a state machine with the blink program, at 2000Hz, with set bound to Pin(25) (LED on the Pico board)
+sm = rp2.StateMachine(0, blink, freq=2000, set_base=Pin(25))
 
 # Run the state machine for 3 seconds.  The LED should blink.
 sm.active(1)
