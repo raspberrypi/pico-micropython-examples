2a402efc9bd05ed9c18a74780c569033d4c93f6e
Andrew Scheller <andrew.scheller@raspberrypi.com>
2025-03-17 16:09:03 +0000

    Fix #70

--- wireless/webserver.py
+++ wireless/webserver.py
@@ -40,10 +40,12 @@ else:
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 
 s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(addr)
 s.listen(1)
 
 print('listening on', addr)
+stateis = "LED is ?"
 
 # Listen for connections
 while True:
